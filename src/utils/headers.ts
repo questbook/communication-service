0d290465f8a4f12f409df1454ac92d8d6f50982a
Richik Chanda <richikchanda1999@gmail.com>
2022-05-13 00:30:15 +0530

    Fix CSRF Error

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { Headers } from "cross-fetch";
 
 const authHeaders = new Headers();
-authHeaders.append("api_key", process.env.DISCOURSE_API_KEY!);
-authHeaders.append("api_username", process.env.DISCOURSE_API_USERNAME!);
+authHeaders.append("api-key", process.env.DISCOURSE_API_KEY!);
+authHeaders.append("api-username", process.env.DISCOURSE_API_USERNAME!);
 authHeaders.append("Content-Type", "application/json");
 
 const defaultHeaders = new Headers();
